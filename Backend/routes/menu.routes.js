03ed4e8549615add7c52b109579df1dc0874719b
Snehanshu Pal <palsnehanshu27@gmail.com>
2024-04-13 19:08:02 +0530

    backend at 13/4

--- Backend/routes/menu.routes.js
+++ Backend/routes/menu.routes.js
@@ -7,7 +7,7 @@ router.get('/ingridients/:id', [authMiddleware.verifyToken], menuController.getM
 router.get('/', [authMiddleware.verifyToken], menuController.getMenuItems);
 router.get('/:id', [authMiddleware.verifyToken], menuController.getMenuItem);
 router.post('/', [authMiddleware.verifyToken, authMiddleware.isManager], menuController.createMenuItem);
-router.put('/:id', [authMiddleware.verifyToken, authMiddleware.isManager], menuController.updateMenuItem);
+router.put('/:id', [authMiddleware.verifyToken], menuController.updateMenuItem);
 router.delete('/:id', [authMiddleware.verifyToken, authMiddleware.isManager], menuController.deleteMenuItem);
 
 export default router;
